a25f8c2e7a439656b498f7af3d8033d7ae45f840
Ryan Bazzell <rybazzell2002@gmail.com>
2024-01-30 09:41:16 -0500

    set up outline and created test code

--- main.java
+++ main.java
@@ -2,6 +2,18 @@ class Main {
     public static void main(String[] args) {
         System.out.println("Goodbye World, Hello Jesus :)");
     }
+
+    private static int[][] thompson_const(String re) {
+        return new int[3][4]; //just a temp line to make it less angry
+    }
+
+    private static int[][] subset_const(int[][] nfa) {
+        return new int[3][4];
+    }
+
+    private static int[][] state_minimization(int[][] dfa) {
+        return new int[3][4];
+    }
 }
 
 //Yes debug make me :)
